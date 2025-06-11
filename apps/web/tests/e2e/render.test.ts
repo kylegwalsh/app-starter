8b3f18e28e8ced7f143ebc33e385d07fd642a69f
Kyle Walsh <kylemco15@gmail.com>
2025-06-11 09:02:33 -0400

    Updated e2e test to use relative url

--- apps/web/tests/e2e/render.test.ts
+++ apps/web/tests/e2e/render.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('app loads and displays the homepage', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('/');
 
   await expect(page).toHaveTitle(/./);
 });
