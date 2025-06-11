de15d6fe4f2cf85f667252733d1ae397e0da9ccd
Kyle Walsh <kylemco15@gmail.com>
2025-06-11 00:02:25 -0400

    Ensuring E2E tests work in CI

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -4,42 +4,44 @@ import { Resource } from 'sst';
 // Sets up our playwright test environment
 // https://playwright.dev/docs/test-configuration
 export default defineConfig({
-  // Look for test files in the "tests" directory, relative to this configuration file.
+  // Look for test files in the "tests" directory, relative to this configuration file
   testDir: 'tests/e2e',
   testMatch: '*.test.ts',
-  // Run all tests in parallel.
+  // Run all tests in parallel
   fullyParallel: true,
-  // Fail the build on CI if you accidentally left test.only in the source code.
+  // Fail the build on CI if you accidentally left test.only in the source code
   forbidOnly: !!process.env.CI,
-  // Retry on CI only.
+  // Retry on CI only
   retries: process.env.CI ? 2 : 0,
-  // Opt out of parallel tests on CI.
+  // Opt out of parallel tests on CI
   workers: process.env.CI ? 1 : undefined,
   // Reporter to use
   reporter: 'html',
   use: {
-    // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: 'http://localhost:3000',
-    // Collect trace when retrying the failed test.
+    // Base URL to use in actions like `await page.goto('/')`
+    // For
+    baseURL: process.env.CI ? Resource.web.url : 'http://localhost:3000',
+    // Collect trace when retrying the failed test
     trace: 'on-first-retry',
   },
-  // Configure projects for major browsers.
+  // Configure projects for major browsers
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-  // Run your local dev server before starting the tests.
-  webServer: {
-    cwd: '../..',
-    command: 'pnpm sst dev',
-    // In our CI, we will test using the deployed URL
-    url: process.env.CI ? Resource.web.url : 'http://localhost:3000',
-    // Don't allow the CI to spin up a new instance if it can't find the deployment
-    reuseExistingServer: process.env.CI ? false : true,
-    stdout: 'pipe',
-    stderr: 'pipe',
-    timeout: 180_000, // 3 minutes
-  },
+  // In CI, don't configure webServer - just use baseURL for the deployed app
+  webServer: process.env.CI
+    ? undefined
+    : {
+        // Run your local dev server before starting the tests
+        cwd: '../..',
+        command: 'pnpm sst dev',
+        port: 3000,
+        reuseExistingServer: true,
+        stdout: 'pipe',
+        stderr: 'pipe',
+        timeout: 180_000, // 3 minutes
+      },
 });
