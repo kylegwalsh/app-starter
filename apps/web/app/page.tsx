7408dcfdc2028c9cdd41437be0c8673d36308500
Kyle Walsh <kylemco15@gmail.com>
2025-07-07 21:49:53 -0400

    Added logic to clean source maps after deploying

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 
-import { analytics } from '@repo/analytics';
 import { Button } from '@repo/design';
 
 import { trpc } from '@/core';
+
+function myErrorMethod() {
+  throw new Error('test');
+}
 
 export default function Page() {
   const { data, isLoading } = trpc.test.useQuery();
@@ -13,7 +16,10 @@ export default function Page() {
     <div className="flex min-h-svh items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-primary text-2xl font-bold">Counting: {isLoading ? '...' : data}</h1>
-        <Button onClick={() => void analytics.captureException(new Error('test'), { test: true })}>
+        <Button
+          onClick={() => {
+            myErrorMethod();
+          }}>
           Frontend Error
         </Button>
         <Button onClick={() => error()}>Backend Error</Button>
