ec33adf1c4ea62b0a0135ac3f03aff1db6e256de
Kyle Walsh <kylemco15@gmail.com>
2026-02-12 23:44:39 -0500

    Added tRPC route types to web

--- apps/web/core/trpc.ts
+++ apps/web/core/trpc.ts
@@ -1,4 +1,7 @@
 import type { AppRouter } from '../../backend/routes';
 import { createTRPCReact } from '@trpc/react-query';
+import type { inferRouterOutputs } from '@trpc/server';
 
 export const trpc = createTRPCReact<AppRouter>();
+
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
