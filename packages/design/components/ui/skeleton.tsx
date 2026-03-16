16fd3b2a164f406fb83b05de7baba646a8ebb75f
Kyle Walsh <kylemco15@gmail.com>
2026-03-16 19:35:27 -0400

    Added better skeleton pulse

--- packages/design/components/ui/skeleton.tsx
+++ packages/design/components/ui/skeleton.tsx
@@ -4,7 +4,7 @@ import type { ComponentProps } from 'react';
 function Skeleton({ className, ...props }: ComponentProps<'div'>) {
   return (
     <div
-      className={cn('animate-pulse rounded-md bg-accent', className)}
+      className={cn('animate-pulse [animation-duration:1.5s] rounded-md bg-accent', className)}
       data-slot="skeleton"
       {...props}
     />
